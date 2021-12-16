8770cd7e51fc9518d4be444a1176b29ef99f2da6
gaganthapar12309 <gsthapar@gmail.com>
2021-12-16 08:37:57 -0800

    adding test code for animating plots

---
+++ testAnimation.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+
+def inOrbitData():
+    return
+
+def inOrbit():
+    return
+
+def loopData():
+    return
+
+def loop():
+    fig = plt.figure()
+    ax = plt.axes(projection = '3d')
+
+if __name__ == '__main__':
+    loop()
+    # inOrbit()
